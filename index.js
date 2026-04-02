ea6c0c49cc6de1bbcfeec5fa47cd65b29145e666
VictoSN <victorionanda1105@gmail.com>
2026-04-02 08:46:21 +0800

    added done and edit functionalities

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const todoList = document.getElementById("todo-list");
-const textField = document.getElementById("todo-text");
+const userTextField = document.getElementById("todo-text");
 const submitButton = document.getElementById("todo-submit");
 
 function createTodo(counterId, textValue) {
     return `
         <div class="counter-element" data-id="id-${counterId}">
-            <text>${textValue}</text>
+            <input class="text-area" value="${textValue}"></input>
             <button class="done-button">Done</button>
+            <button class="delete-button">Delete</button>
         </div>
     `;
 }
@@ -15,15 +16,31 @@ function deleteTodo(newTodo) {
     newTodo.remove();
 }
 
+function doneTodo(newTodo) {
+    const todoText = newTodo.querySelector(".text-area")
+    const doneButton = newTodo.querySelector(".done-button");
+    if (doneButton.textContent === "Done") {
+        doneButton.textContent = "Undone";
+        todoText.classList.toggle("strike");
+        todoText.disabled = true;
+    } else {
+        doneButton.textContent = "Done";
+        todoText.classList.toggle("strike");
+        todoText.disabled = false;
+    }
+}
+
 function setupEventListener(newTodo) {
     const doneButton = newTodo.querySelector(".done-button");
+    const deleteButton = newTodo.querySelector(".delete-button");
 
-    doneButton.addEventListener("click", () => deleteTodo(newTodo));
+    doneButton.addEventListener("click", () => doneTodo(newTodo));
+    deleteButton.addEventListener("click", () => deleteTodo(newTodo));
 }
 
 function insertTodo() {
     const counterId = document.querySelectorAll('.counter-element').length + 1;
-    const textValue = textField.value;
+    const textValue = userTextField.value;
     todoList.insertAdjacentHTML("beforeend", createTodo(counterId, textValue));
     setupEventListener(todoList.lastElementChild);
 }
